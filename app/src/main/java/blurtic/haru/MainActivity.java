d48315a629e3f6056a77f9c78fabbf143eab7c90
Warguss <kuc0104@naver.com>
2016-06-17 17:19:59 +0900

    asdf

--- app/src/main/java/blurtic/haru/MainActivity.java
+++ app/src/main/java/blurtic/haru/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //TODO : 시발
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
